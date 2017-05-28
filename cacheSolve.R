22aee3a7acf05d40cc1f470ba7ed0f21a9cd8655
Octavio Alberto Cortes Torres <octaviocortest@gmail.com>
2017-05-28 15:37:09 -0500

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,4 +1,11 @@
 cacheSolve <- function(x, ...) {
+    ## The 2nd function below, cacheSolve calculates the Inverse of 
+  ## the special "matrix" created with the 1st function.
+    # It first checks to see if the Inverse has already been calculated
+    # If so, it gets the inverse from the cache and skips the computation
+    # Otherwise, it calculates the Inverse of the matrix and 
+    # sets the value of the Inverse in the cache via the setInverse function.
+  
   m <- x$getinv()
   if(!is.null(m)) {
     message("getting cached data")
